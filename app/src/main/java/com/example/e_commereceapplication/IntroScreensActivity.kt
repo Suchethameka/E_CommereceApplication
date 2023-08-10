2727a10a5433455def76eb55c3aec6fd69147265
Suchetha <suchetha.ls177@gmail.com>
2023-08-10 18:05:38 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/e_commereceapplication/IntroScreensActivity.kt
@@ -0,0 +1,11 @@
+package com.example.e_commereceapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IntroScreensActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro_screens)
+    }
+}
